0c43e0ea4cee182823f3f77fef7a1d86234b7fbc
kiannadalton <kiannadalton@aol.com>
2025-08-28 18:35:28 -0400

    Completed Guided Practice

--- Guided Practice/index.js
+++ Guided Practice/index.js
@@ -6,11 +6,21 @@ const state = {
 
 /** Moves a sheep from start to target */
 function moveSheep() {
-  // TODO
+  //sheep is the clicked on button that is removed from the state.start array and that value is moved to the state target array.
+  const sheep = state.start.pop();
+  state.target.push(sheep);
+
+  //re-renders the starting and target banks
+  render();
 }
 
 // === Render ===
 
+const form = document.querySelector("form");
+const startingBank = document.querySelector("#startingBank ul");
+const targetBank = document.querySelector("#targetBank ul");
+
+form.addEventListener("submit", addSheep);
 /** Renders sheep on the starting bank */
 function renderStartSheep() {
   const startingSheep = state.start.map((sheep) => {
@@ -21,17 +31,25 @@ function renderStartSheep() {
     li.append(button);
 
     // TODO: Add event listener so the sheep moves when clicked
+    button.addEventListener("click", moveSheep);
 
     return li;
   });
-
-  const startingBank = document.querySelector("#startingBank ul");
+  // iterates through all of the sheep in the array
   startingBank.replaceChildren(...startingSheep);
 }
 
 /** Renders sheep on the target bank */
 function renderTargetSheep() {
   // TODO
+  //map creates a new array based on the sheep added to its array from the moveSheep array
+  const targetSheep = state.target.map((sheep) => {
+    //
+    const element = document.createElement("li");
+    element.textContent = "🐑";
+    return element;
+  });
+  targetBank.replaceChildren(...targetSheep);
 }
 
 function render() {
@@ -44,3 +62,18 @@ function render() {
 render();
 
 // TODO: Add sheep to the starting bank when the form is submitted
+
+function addSheep(event) {
+  // prevents page from refreshing
+  event.preventDefault();
+
+  const numSheep = form.elements.numSheep.value;
+  //numSheep is the input value. the below loop adds a sheep for every increment in the input value to the start array within the state object.
+  for (let i = 0; i < numSheep; i++) {
+    state.start.push("sheep");
+  }
+
+  render();
+  //clears the form
+  form.elements.numSheep.value = null;
+}
